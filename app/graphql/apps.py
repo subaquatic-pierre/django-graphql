e1f47d90ed5fabdac757ece78e143f6301ecf646
Pierre <subaquatic.pierre@gmail.com>
2021-04-20 14:54:36 +0400

    Completed Create, List and Read single todo

--- app/graphql/apps.py
+++ app/graphql/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class NewConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "graphql"
+    name = "app.graphql"
--- app/graphql/mutations.py
+++ app/graphql/mutations.py
@@ -1,5 +1,7 @@
 import graphene
 import graphene_django
+from .types import TodoType
+from ..core.models import Todo
 
 
 class CreateTodo(graphene.Mutation):
@@ -8,3 +10,18 @@ class CreateTodo(graphene.Mutation):
         content = graphene.String(required=False)
         completed = graphene.Boolean(required=False)
 
+    todo = graphene.Field(TodoType)
+
+    def mutate(self, info, **kwargs):
+        title = kwargs.get("title")
+        content = kwargs.get("content")
+        completed = kwargs.get("completed")
+
+        todo = Todo(title=title, content=content, completed=completed)
+        todo.save()
+
+        return CreateTodo(todo)
+
+
+class TodoMutations(graphene.ObjectType):
+    create_todo = CreateTodo.Field()
