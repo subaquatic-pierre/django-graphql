7838a0259054153d5b9d5c9852ef51154ef6df74
Pierre <subaquatic.pierre@gmail.com>
2021-04-21 12:21:27 +0400

    Created todo query tests

--- app/graphql/tests.py
+++ app/graphql/tests.py
@@ -1,3 +1,53 @@
-from django.test import TestCase
+import json
+from graphene_django.utils.testing import GraphQLTestCase
+from .schema import schema
 
-# Create your tests here.
+
+class TestTodoQueries(GraphQLTestCase):
+    GRAPHQL_SCHEMA = schema
+    fixtures = ["todo"]
+
+    def setUp(self) -> None:
+        return super().setUp()
+
+    def test_get_todos(self):
+        response = self.query(
+            """ 
+          query GetTodos {
+            todos {
+                id
+                title
+                content
+                completed
+            }
+        }
+        """,
+            op_name="GetTodos",
+        )
+
+        self.assertResponseNoErrors(response)
+        content = json.loads(response.content)
+        todos = content.get("data").get("todos")
+        assert len(todos) >= 0, "No todos were returned from the query"
+
+    def test_get_todo(self):
+        response = self.query(
+            """ 
+          query GetTodo($id: ID) {
+            todo(id: $id) {
+                id
+                title
+                content
+                completed
+            }
+        }
+        """,
+            op_name="GetTodo",
+            variables={"id": 1},
+        )
+
+        self.assertResponseNoErrors(response)
+        content = json.loads(response.content)
+        todo = content.get("data").get("todo")
+        assert todo.get("title") == "Create New world", "Incorrect todo returned"
+
